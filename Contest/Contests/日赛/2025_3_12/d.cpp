739072441efb229824b2faaaf00fab6a172198f2
1987873567 <1987873567@qq.com>
2025-03-12 20:32:54 +0800

    Auto commit on 20250312_203254

--- Contest/Contests/日赛/2025_3_12/d.cpp
+++ Contest/Contests/日赛/2025_3_12/d.cpp
@@ -10,26 +10,50 @@ using i64 = long long;
 // std::uniform_int_distribution<int> r1(1, 10);
 // constexpr int d4[4][2] = {-1, 0, 0, 1, 1, 0, 0, -1};
 // constexpr int d8[8][2] = {-1, 0, -1, 1, 0, 1, 1, 1, 1, 0, 1, -1, 0, -1, -1, -1};
+/*
+13 8 5 4
+3 4 2 1
 
+10 4 3 3
+3 4 2 1
+
+
+*/
 void solve() {
-	i64 n, x;
-	std::cin >> n >> x;
-	std::vector<i64> v(n + 1), dp(n + 1, -1e10);
+	int n;
+	std::cin >> n;
+	std::vector<i64> v(n + 2), a(n + 2), sum(n + 2), ans(n + 2), x(n + 2);
 	for(int i = 1; i <= n; i++){
 		std::cin >> v[i];
-		v[i] += v[i - 1];
 	}
 	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= i; j++){
-			dp[i - j + 1] = std::max(dp[i - j + 1], v[i] - v[j - 1]);
+		std::cin >> a[i];
+		sum[i] = sum[i - 1] + a[i];
+	}
+	for(int i = 1; i <= n; i++){
+		int l = i, r = n + 1;
+		while(l + 1 != r){
+			int m = (l + r) / 2;
+			i64 t = sum[m] - sum[i - 1];
+			if(t <= v[i]){
+				l = m;
+			} else {
+				r = m;
+			}
+		}
+		i64 pos = l;
+		if(sum[pos] - sum[i - 1] <= v[i]){
+			x[i] += 1;
+			x[pos + 1] --;
+			ans[pos + 1] += std::min(a[pos + 1], v[i] - sum[pos] + sum[i - 1]);
+		} else {
+			ans[pos] += std::min(v[i], a[pos]);
 		}
 	}
-	for(int i = 0; i <= n; i++){
-		i64 ans = 0;
-		for(int j = 1; j <= n; j++){
-			ans = std::max(ans, dp[j] + x * std::min(i, j));
-		}
-		std::cout << ans << " \n"[i == n];
+	for(int i = 1; i <= n; i++){
+		x[i] += x[i - 1];
+		ans[i] += x[i] * a[i];
+		std::cout << ans[i] << " \n"[i == n];
 	}
 }
 
